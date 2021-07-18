21c7aff24a08f5a8bf6418154ecf1e06237d03ec
Marc Frei <marc.frei@gmail.com>
2021-07-18 15:10:02 +0200

    Enable sciond access via infra interfaces in testnet namespaces for LFs

--- testnet/testnet-down.sh
+++ testnet/testnet-down.sh
@@ -8,6 +8,10 @@ sudo ip netns del far-0
 sudo ip netns del near-0
 sudo ip link del one
 
+sudo ip link del infra1
+
 sudo ip netns del far-1
 sudo ip netns del near-1
 sudo ip link del five
+
+sudo ip link del infra5
